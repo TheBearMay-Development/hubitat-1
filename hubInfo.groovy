b3ab3023f37091c2623d7d34d1bb15b28e5b8e10
thebearmay <32334899+thebearmay@users.noreply.github.com>
2021-03-05 15:19:46 -0500

    Update hubInfo.groovy

--- hubInfo.groovy
+++ hubInfo.groovy
@@ -19,11 +19,12 @@
  *    2021-01-31  thebearmay     Code cleanup, release ready
  *    2021-01-31  thebearmay     Putting a config delay in at initialize to make sure version data is accurate
  *    2021-02-16  thebearmay     Add text date for restart
- *    2021-03-04  thebearmay     Added CPU and Temperature polling
- *    2021-03-05  thebearmay     Add the degree symbol and scale to the temperature attribute 
+ *    2021-03-05  thebearmay     Added CPU and Temperature polling
+ *    2021-03-05  lgkhan	 Added new formatted uptime attr, also added an html attr that stores a bunch of the usefull 
+ *					info in table format so you can use on any dashboard
  */
 import java.text.SimpleDateFormat
-static String version()	{  return '1.3.4'  }
+static String version()	{  return '1.3.3'  }
 
 metadata {
     definition (
@@ -59,8 +60,9 @@ metadata {
         attribute "locationId", "string"
         attribute "lastHubRestartFormatted", "string"
         attribute "freeMemory", "string"
-        attribute "temperatureF", "string"
-        attribute "temperatureC", "string"
+        attribute "formattedUptime", "string"
+        attribute "html", "string";                              
+   
 		command "configure"
             
     }   
@@ -70,6 +72,7 @@ preferences {
 	input("debugEnable", "bool", title: "Enable debug logging?")
     input("tempPollEnable", "bool", title: "Enable Temperature Polling")
     input("tempPollRate", "number", title: "Temperature Polling Rate (seconds)\nDefault:300", default:300, submitOnChange: true)
+    input("attribEnable", "bool", title: "Enable Info attribute?", default: false, required: false, submitOnChange: true)
 }
 
 def installed() {
@@ -87,11 +90,13 @@ def configure() {
     for(i=0;i<locProp.size();i++){
         updateAttr(locProp[i], location["${locProp[i]}"])
     }
+    formatUptime()
     updateAttr("hubVersion", location.hub.firmwareVersionString) //retained for backwards compatibility
     updateAttr("locationName", location.name)
     updateAttr("locationId", location.id)
     updateAttr("lastUpdated", now())
     if (tempPollEnable) getTemp()
+    if (attribEnable) formatAttrib()
 }
 
 def updateAttr(aKey, aValue){
@@ -108,6 +113,77 @@ def initialize(){
     runIn(30,configure)
 }
 
+def formatUptime()
+
+{
+  String attrval 
+
+    Integer ut = device.currentValue("uptime").toDouble()
+    Integer days = (ut/(3600*24))
+    Integer hrs = (ut - (days * (3600*24))) /3600
+    Integer min =  (ut -  ((days * (3600*24)) + (hrs * 3600))) /60
+    Integer sec = ut -  ((days * (3600*24)) + (hrs * 3600) + (min * 60))
+    
+    attrval = days.toString() + " days, " + hrs.toString() + " hours, " + min.toString() + " minutes and " + sec.toString() + " seconds."
+    sendEvent(name: "formattedUptime", value: attrval, isChanged: true) 
+}
+
+def formatAttrib()
+{ 
+    state.attrString = "<table>"
+  
+   def currentState = state.attrString
+   def result1 = addToAttr("Name","name")
+   def result2 = addToAttr("Version","hubVersion")
+   def result3 = addToAttr("Address","localIP")
+   def result4 = addToAttr("Free Memory","freeMemory","int")
+   def result5 = addToAttr("Last Restart","lastHubRestartFormatted")
+   def result6 = addToAttr("Uptime","formattedUptime")
+   def result7 = addTempToAttr("Temperature","temperature","temperatureScale")
+    
+    state.attrString = currentState + result1 + result2 + result3 + result4 + result5 + result6 + result7 + "</table>"
+   
+   // log.debug "after calls attr string = $state.attrString"
+    sendEvent(name: "html", value: state.attrString, isChanged: true)
+}
+
+def addTempToAttr(String name, String key, String scaleKey)
+{
+   // log.debug "adding $name, $key"
+    String retResult
+    retResult = '<Tr><td align="left">'
+    retResult = retResult + name + '</td><td space="5"> </td><td align="left">'
+    String attrval = device.currentValue(key)
+    String scale = device.currentValue(scaleKey)
+    
+    retResult = retResult + attrval + " °" + scale
+    retResult = retResult + '</td></tr>'
+  
+    retResult  
+}
+
+def addToAttr(String name, String key, String convert = "none")
+{
+   // log.debug "adding $name, $key"
+    String retResult
+    retResult = '<Tr><td align="left">'
+    retResult = retResult + name + '</td><td space="5"> </td><td align="left">'
+    String attrval 
+    
+    if (convert == "int")
+    {      
+    Integer temp = device.currentValue(key).toDouble()
+    attrval = temp.toString()
+    }
+    
+   else attrval = device.currentValue(key)
+    
+    retResult = retResult + attrval
+    retResult = retResult + '</td></tr>'
+  
+    retResult
+}
+    
 def getTemp(){
     params = [
         uri: "http://${location.hub.localIP}:8080",
@@ -119,16 +195,15 @@ def getTemp(){
             response.headers.each {
                 log.debug "${it.name} : ${it.value}"
             }
+            
             log.debug response.data
         }
         tempWork = new Double(response.data.toString())
         if(debugEnable) log.debug tempWork
         if (location.temperatureScale == "F")
-            sendEvent(name:"temperature",value:celsiusToFahrenheit(tempWork),unit:"°${location.temperatureScale}")
+            updateAttr("temperature",celsiusToFahrenheit(tempWork))
         else
-            sendEvent(name:"temperature",value:tempWork,unit:"°${location.temperatureScale}")
-        updateAttr("temperatureF",celsiusToFahrenheit(tempWork)+ "<span class='small'> °F</span>")
-        updateAttr("temperatureC",tempWork+ "<span class='small'> °C</span>")
+            updateAttr("temperature",tempWork)
     })
     
     // get Free Memory
